5bd1c56af1da906b910f3b2e08cdceddb146a51e
Nemanja Radivojevic <nemanjaradivojevic1603@gmail.com>
2022-06-02 14:03:13 +0200

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link';
-import Image from 'next/Image';
+import Image from 'next/image';
 import logo from '../public/disney.png';
 
 function NavBar({account}) {
